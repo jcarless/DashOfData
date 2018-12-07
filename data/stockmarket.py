f7bd3bbd5e90eb864bbbc2d09320063e93b7ab52
Jerome Carless <carless.jerome@gmail.com>
2018-12-07 13:17:36 -0500

    added data folder to hold examples of api usage

---
+++ data/stockmarket.py
@@ -0,0 +1,10 @@
+from alpha_vantage.sectorperformance import SectorPerformances
+import matplotlib.pyplot as plt
+
+sp = SectorPerformances(key='YOUR_API_KEY', output_format='pandas')
+data, meta_data = sp.get_sector()
+data['Rank A: Real-Time Performance'].plot(kind='bar')
+plt.title('Real Time Performance (%) per Sector')
+plt.tight_layout()
+plt.grid()
+plt.show()
---
+++ data/unemployment.py
@@ -0,0 +1,25 @@
+import requests
+import json
+import prettytable
+headers = {'Content-type': 'application/json'}
+data = json.dumps({"seriesid": ['LAUCN281070000000003'],"startyear":"2011", "endyear":"2014"})
+p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
+json_data = json.loads(p.text)
+# print(json_data)
+for series in json_data['Results']['series']:
+    # print("SERIES: ", series)
+    x=prettytable.PrettyTable(["series id","year","period","value","footnotes"])
+    seriesId = series['seriesID']
+    for item in series['data']:
+        year = item['year']
+        period = item['period']
+        value = item['value']
+        footnotes=""
+        for footnote in item['footnotes']:
+            if footnote:
+                footnotes = footnotes + footnote['text'] + ','
+        if 'M01' <= period <= 'M12':
+            x.add_row([seriesId,year,period,value,footnotes[0:-1]])
+    output = open(seriesId + '.txt','w')
+    output.write (x.get_string())
+    output.close()
