594d70e6dc78b17b906ec76adbe521e347eb31d7
Jerome Carless <carless.jerome@gmail.com>
2018-12-10 11:25:43 -0500

    update alpha_vantage demo code

--- data/stockmarket.py
+++ data/stockmarket.py
@@ -1,10 +1,9 @@
 from alpha_vantage.sectorperformance import SectorPerformances
 import matplotlib.pyplot as plt
 
-sp = SectorPerformances(key='YOUR_API_KEY', output_format='pandas')
+sp = SectorPerformances(key='7WPQAG2NRC8PLFIZ', output_format='pandas')
 data, meta_data = sp.get_sector()
-data['Rank A: Real-Time Performance'].plot(kind='bar')
-plt.title('Real Time Performance (%) per Sector')
-plt.tight_layout()
-plt.grid()
-plt.show()
+print(data['Rank D: Month Performance']["Consumer Discretionary"]) # 1 month
+print(data['Rank E: Month Performance']["Consumer Discretionary"]) # 3 month
+print(data['Rank F: Year-to-Date (YTD) Performance']["Consumer Discretionary"]) # ytd
+print(data['Rank G: Year Performance']["Consumer Discretionary"]) # 1 year
